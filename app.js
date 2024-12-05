b3eaf86248e848c11b2003ea610d6a4ed4c59cbb
sabbirRshuvo <sabbirhshuvo11@gmail.com>
2024-12-05 15:44:33 +0600

    projects is almost

--- app.js
+++ app.js
@@ -1,47 +1,96 @@
 const noSeatBookEl = document.getElementById("no-seat-booked");
 const seatCountEl = document.getElementById("seat-count");
 const seatAvailableEl = document.getElementById("available-seat");
-const totalSeatPrice = document.getElementById("total-price");
+const totalSeatPriceEl = document.getElementById("total-price");
+
 const couponInputEl = document.getElementById("coupon-input");
 const couponBtnEl = document.getElementById("coupon-btn");
 
+const noSeatHidden = document.getElementById("no-seat");
+
+const couponInputValue = document.getElementById("coupon-input");
+
+const grandTotalEl = document.getElementById("grand-total");
+
+const phoneNumber = document.getElementById("phone-number");
+const nextButton = document.getElementById("next-btn");
+
 let seatIncrease = [];
-let ticketPrice = 0;
+let totalSeatPrice = 0;
 function handleButtonSeat(event) {
   const value = event.innerText;
-
   if (seatIncrease.includes(value)) {
-    return alert("seat already added");
+    return alert("already seat is added");
   } else if (seatIncrease.length < 4) {
-    //   set color
-    event.classList.add("bg-green-400");
+    event.classList.add("bg-green-500");
     event.classList.add("text-white");
 
-    //   seat increase
+    // increase
     seatIncrease.push(event.innerText);
     seatCountEl.innerText = seatIncrease.length;
 
-    //   seat descrase
+    // decrase
     const previousSeat = seatAvailableEl.innerText;
     const currentSeat = previousSeat - 1;
     seatAvailableEl.innerText = currentSeat;
-    //
-    noSeatBookEl.innerHTML += `
-  <p class="flex justify-between text-black">
-  <span>${event.innerText}</span>
-  <span>Economy</span>
-  <span>550</span>
-  </p>
-  `;
-    ticketPrice += 550;
-    totalSeatPrice.innerText = ticketPrice.toFixed(2);
 
-    //   coupon
+    noSeatHidden.classList.add("hidden");
+    noSeatBookEl.innerHTML += `
+    <p class="flex justify-between">
+    <span>${event.innerText}</span>
+    <span>Economy</span>
+    <span>550</span>
+    </p>
+    `;
+    // remove hidden
+
+    totalSeatPrice += 550;
+    totalSeatPriceEl.innerText = totalSeatPrice.toFixed(2);
+
+    // coupon
+
     if (seatIncrease.length > 3) {
       couponInputEl.removeAttribute("disabled");
       couponBtnEl.removeAttribute("disabled");
     }
   } else {
-    return alert("maximum seat added");
+    return alert("maximum seat is added");
   }
 }
+
+document.getElementById("coupon-btn").addEventListener("click", function () {
+  let save = 0;
+  const couponTotalValue = couponInputValue.value;
+  if (couponTotalValue !== "NEW50" && couponTotalValue !== "couple 20") {
+    return alert("wrong pin");
+  }
+  if (couponTotalValue === "NEW50") {
+    save = totalSeatPrice * 0.15;
+  }
+  if (couponTotalValue === "couple 20") {
+    save = totalSeatPrice * 0.2;
+  }
+
+  const showCuoponElement = document.getElementById("hide-coupon");
+  showCuoponElement.innerHTML = `
+  <p>Discount</p>
+  <p>
+  <span>-BDT.</span>
+  <span>${save.toFixed(2)}</span>
+  </p>
+  `;
+
+  const grandTotalValues = totalSeatPrice - save;
+  grandTotalEl.innerText = grandTotalValues.toFixed(2);
+});
+
+phoneNumber.addEventListener("input", function (event) {
+  const inputValues = event.target.value;
+  if (inputValues.length >= 11) {
+    nextButton.removeAttribute("disabled");
+  }
+});
+
+document.getElementById("btn-continue").addEventListener("click", function () {
+  window.location.reload();
+});
